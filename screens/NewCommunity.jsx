17dc72f6da2843dca6803a3d88d09b1ed44b68ad
cvrles15 <carles.marcilla@boehringer-ingelheim.com>
2024-04-18 16:21:46 +0200

    profile,community

--- screens/NewCommunity.jsx
+++ screens/NewCommunity.jsx
@@ -1,8 +1,197 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, ScrollView, Pressable } from 'react-native'
+import Icon from 'react-native-vector-icons/AntDesign'; 
+import mando from '../assets/mando.jpg';
 
 export const NewCommunity = () => {
   return (
-    <Text>NewCommunity desd branch</Text>
-  )
+    <View style={styles.crearComunidadAutoLayout}>
+      <View style={styles.bienvenidoFrame}>
+        <Text style={styles.bienvenidoNombreUsuario}>Bienvenido, nombreUsuario</Text>
+        <Image source={mando} style={styles.ellipse4} />
+      </View>
+      <View style={styles.boxComunidadFrame}>
+        <Icon name="stepbackward" style={styles.returnButtonFrame} />
+        <Text style={styles.crearComunidad}>CREAR COMUNIDAD</Text>
+        <Text style={styles.nombreDeLaComunidad}>Nombre de la Comunidad:</Text>
+        <View style={styles.nombreComunidadBox}></View>
+        <Text style={styles.indicaLaCategoriaDeLaComunidad}>Indica la categoria de la comunidad:</Text>
+        <View style={styles.nombreComunidadBox2}>
+          <Icon name="down" style={styles.deployButton} />
+        </View>
+        <Text style={styles.aAdeFotosDeTuComunidad}>Añade fotos de tu comunidad:</Text>
+        <Icon name="clouduploado" style={styles.uploadButtonFrame} />
+        <Text style={styles.linkUrlDeTuChatPrivadoDeLaComunidad}>Link/Url de tu chat privado de la comunidad:</Text>
+        <View style={styles.linkComunidadBox}></View>
+        <Text style={styles.descripcionDeTuComunidad}>Descripción de tu comunidad:</Text>
+        <View style={styles.descripcionComunidadBox}></View>
+        <View style={styles.frame33}>
+          <View style={styles.group20}>
+            <View style={styles.rectangle32}></View>
+            <Text style={styles.crear}>Crear</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
+const styles = {
+  crearComunidadAutoLayout: {
+    backgroundColor: '#cee4ca',
+    borderWidth: 1,
+    borderColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 852,
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  bienvenidoFrame: {
+    backgroundColor: '#669579',
+    paddingVertical: 3,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: 407,
+    height: 60,
+  },
+  bienvenidoNombreUsuario: {
+    color: '#ffecec',
+    textAlign: 'left',
+    fontFamily: 'Inter-Regular',
+    fontSize: 18,
+    fontWeight: '400',
+    width: 211,
+  },
+  ellipse4: {
+    width: 51,
+    height: 44,
+  },
+  boxComunidadFrame: {
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    padding: 10,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    width: 379,
+    height: 743,
+  },
+  returnButtonFrame: {
+    borderRadius: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 7,
+  },
+  crearComunidad: {
+    color: '#000000',
+    textAlign: 'left',
+    fontFamily: 'Inter-Regular',
+    fontSize: 22,
+    fontWeight: '400',
+    width: 267,
+    height: 27,
+  },
+  nombreDeLaComunidad: {
+    color: '#000000',
+    textAlign: 'left',
+    fontFamily: 'Inter-Regular',
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  nombreComunidadBox: {
+    backgroundColor: '#eeeded',
+    borderRadius: 5,
+    height: 27,
+  },
+  indicaLaCategoriaDeLaComunidad: {
+    color: '#000000',
+    textAlign: 'left',
+    fontFamily: 'Inter-Regular',
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  nombreComunidadBox2: {
+    backgroundColor: '#eeeded',
+    borderRadius: 5,
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+    height: 27,
+  },
+  deployButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 5,
+    width: 24,
+    height: 23,
+  },
+  aAdeFotosDeTuComunidad: {
+    color: '#000000',
+    textAlign: 'left',
+    fontFamily: 'Inter-Regular',
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  uploadButtonFrame: {
+    borderRadius: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 7,
+  },
+  linkUrlDeTuChatPrivadoDeLaComunidad: {
+    color: '#000000',
+    textAlign: 'left',
+    fontFamily: 'Inter-Regular',
+    fontSize: 18,
+    fontWeight: '400',
+    width: 310,
+  },
+  linkComunidadBox: {
+    backgroundColor: '#eeeded',
+    borderRadius: 5,
+    height: 27,
+  },
+  descripcionDeTuComunidad: {
+    color: '#000000',
+    textAlign: 'left',
+    fontFamily: 'Inter-Regular',
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  descripcionComunidadBox: {
+    backgroundColor: '#eeeded',
+    borderRadius: 5,
+    height: 164,
+  },
+  frame33: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 310,
+    height: 29,
+  },
+  group20: {
+    width: 134,
+    height: 29,
+  },
+  rectangle32: {
+    backgroundColor: '#579770',
+    borderRadius: 20,
+    width: 134,
+    height: 29,
+    position: 'absolute',
+    left: 88,
+    top: 0,
+  },
+  crear: {
+    color: '#ffffff',
+    textAlign: 'right',
+    fontFamily: 'Inter-Bold',
+    fontSize: 24,
+    fontWeight: '700',
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+  },
+};
+
+export default NewCommunity;
+
--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,8 +1,171 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign'; 
+import mando from '../assets/mando.jpg';
 
-export const Profile = () => {
+const ConfigProfile = () => {
   return (
-    <Text>Profile</Text>
-  )
-}
+    <View style={styles.configuracionUsuario}>
+      <Icon name="stepbackward" style={styles.returnButtonFrame} />
+      <View style={styles.frame37}>
+        <View style={styles.avatar}>
+          <Image source={mando} style={styles.avatar2} />
+          <Text style={styles.editarFotoDePerfil}>Editar foto de perfil</Text>
+        </View>
+        <View style={styles.frame38}>
+          <View style={styles.input}>
+            <Text style={styles.cambiarNombreDeUsuario}>Cambiar nombre de usuario</Text>
+            <Input style={styles.rectangle15}/>
+          </View>
+          <View style={styles.input}>
+            <Text style={styles.cambiarContrasena}>Cambiar contraseña</Text>
+            <Input style={styles.rectangle15}/>
+          </View>
+          <View style={styles.unirteButton}>
+          <Input style={styles.rectangle15}/>
+            <Text style={styles.guardarCambios}>Guardar cambios</Text>
+          </View>
+          <View style={styles.eliminarBoton}>
+            <View style={styles.rectangle22}></View>
+            <Text style={styles.eliminarCuenta}>Eliminar Cuenta</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = {
+  configuracionUsuario: {
+    backgroundColor: '#d8d8d8',
+    borderWidth: 1,
+    borderColor: '#000000',
+    padding: 20,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    height: 852,
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  returnButtonFrame: {
+    borderRadius: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 7,
+    width: 60,
+    height: 50,
+  },
+  frame37: {
+    paddingVertical: 31,
+    paddingHorizontal: 123,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 402,
+    height: 766,
+  },
+  avatar: {
+    width: 146,
+    height: 137,
+  },
+  avatar2: {
+    width: 125,
+    height: 108,
+    position: 'absolute',
+    left: 138.5,
+    top: 154.5,
+  },
+  editarFotoDePerfil: {
+    color: '#3c664c',
+    fontFamily: 'Inter-Regular',
+    fontSize: 16,
+    position: 'absolute',
+    left: 128,
+    top: 272.5,
+  },
+  frame38: {
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 310,
+  },
+  input: {
+    width: 328,
+    height: 57,
+  },
+  cambiarNombreDeUsuario: {
+    color: '#000000',
+    fontFamily: 'Inter-Regular',
+    fontSize: 16,
+    position: 'absolute',
+    left: 11,
+    top: 10,
+    width: 225,
+    height: 24,
+  },
+  rectangle21: {
+    width: 328,
+    height: 33,
+    position: 'absolute',
+    left: 10,
+    top: 34,
+  },
+  cambiarContrasena: {
+    color: '#000000',
+    fontFamily: 'Inter-Regular',
+    fontSize: 16,
+    position: 'absolute',
+    left: 11,
+    top: 97,
+    width: 164,
+    height: 24,
+  },
+  rectangle212: {
+    width: 328,
+    height: 33,
+    position: 'absolute',
+    left: 10,
+    top: 121,
+  },
+  unirteButton: {
+    width: 215.84,
+    height: 36,
+  },
+  rectangle15: {
+    width: 215.84,
+    height: 36,
+    position: 'absolute',
+    left: 66.08,
+    top: 184,
+  },
+  guardarCambios: {
+    color: '#ffffff',
+    fontFamily: 'Inter-Bold',
+    fontSize: 16,
+    fontWeight: '700',
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+  },
+  eliminarBoton: {
+    width: 234,
+    height: 31,
+  },
+  rectangle22: {
+    backgroundColor: '#ff0000',
+    width: 234,
+    height: 31,
+    position: 'absolute',
+    left: '50%',
+    top: 250,
+    marginLeft: -117,
+  },
+  eliminarCuenta: {
+    color: '#ffffff',
+    fontFamily: 'Inter-Regular',
+    fontSize: 16,
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+  },
+};
+
+export default ConfigProfile;
